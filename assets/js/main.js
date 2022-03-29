098003cbd87aa9d65686e43fd03c14cb57aa20c8
MirkOrlando <mirko@orlando-m.it>
2022-03-29 14:33:43 +0200

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+ */
+
+/* definire la lista di email autorizzate */
+const emailList = [
+  "hazem70@mailpremium.net",
+  "gnuygnuy@hrysyu.com",
+  "igra55555555@icenhl.com",
+  "barrybird@eluvit.com",
+  "moss64@kyverify.ga",
+  "loliwy@burgas.vip",
+];
+
+/* verificare che l'utente sia autorizzato */
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+ */
