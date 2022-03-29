93d673758dbc6d0266269385e288da9b0f977960
MirkOrlando <mirko@orlando-m.it>
2022-03-29 14:52:29 +0200

    created log-in program

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
  */
 
 /* definire la lista di email autorizzate */
-const emailList = [
+const emailsList = [
   "hazem70@mailpremium.net",
   "gnuygnuy@hrysyu.com",
   "igra55555555@icenhl.com",
@@ -12,7 +12,27 @@ const emailList = [
   "loliwy@burgas.vip",
 ];
 
+console.log(emailsList);
+
 /* verificare che l'utente sia autorizzato */
+const userEmail = prompt("inserisci il tuo indirizzo email");
+console.log(userEmail);
+let logged = false;
+
+for (let i = 0; i < emailsList.length; i++) {
+  const email = emailsList[i];
+  /* console.log(email); */
+  if (userEmail === email) {
+    logged = true;
+  }
+  /* console.log(logged); */
+}
+
+if (logged === true) {
+  alert("Accesso eseguito correttamento");
+} else {
+  alert("Utente non riconosciuto, procedere prima alla registrazione");
+}
 
 /* Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
